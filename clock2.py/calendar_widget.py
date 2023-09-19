7f69e0fcec85666c6b867f12e386995ff06f0c43
Mate Bartfai <matthaeusb@gmail.com>
2023-09-19 22:00:08 +0200

    fixing calendar event paint

--- clock2.py/calendar_widget.py
+++ clock2.py/calendar_widget.py
@@ -9,8 +9,8 @@ import sys
 
 COLORS = {
     "EVENT": {
-        "BG": QColor(10, 64, 155),
-        "FONT": QColor(10,10,155),
+        "BG": QColor(10, 64, 155, 25),
+        "FONT": QColor(10,10,10),
     },
 }
 
@@ -26,8 +26,23 @@ class CalendarWidget(QCalendarWidget):
     def init_calendar(self):
         creds = authenticate()
         self.events = get_events(creds)
+        self.events_dates = {QDate(date) for date in self.events}
 
     def paintCell(self, painter: PySide2.QtGui.QPainter, rect: PySide2.QtCore.QRect, date: PySide2.QtCore.QDate) -> None:
+    
+        if date in self.events_dates:
+            painter.save()
+            painter.setPen(COLORS["EVENT"]["FONT"])
+            flags = Qt.TextSingleLine | Qt.AlignCenter
+            painter.drawText(rect, flags, str(date.day()))
+
+            painter.setPen(COLORS["EVENT"]["BG"])
+            painter.drawRect(rect)
+            painter.fillRect(rect, COLORS["EVENT"]["BG"])
+            painter.drawRect(rect)
+        else:
+            super(CalendarWidget, self).paintCell(painter, rect, date)
+    def bak_paintCell(self, painter: PySide2.QtGui.QPainter, rect: PySide2.QtCore.QRect, date: PySide2.QtCore.QDate) -> None:
         for event_date, summary in self.events.items():
 
             if date == QDate(event_date):
