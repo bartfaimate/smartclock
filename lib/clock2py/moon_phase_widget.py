f7686552cfda24ac1cefdd992cfd3f72bdc7d209
mate <matthaeusb@gmail.com>
2024-02-04 17:31:34 +0100

    moon api widget

---
+++ lib/clock2py/moon_phase_widget.py
@@ -0,0 +1,110 @@
+from cachetools import cached, LRUCache
+import datetime as dt
+import sys
+import typing
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+from geopy.geocoders import Nominatim
+from PySide2.QtCore import Qt, QTimer, QTime, QRect, Slot, QEvent
+from PySide2.QtGui import QFont, QPainter, QPalette, QColor, QPen, QPaintEvent 
+from PySide2.QtWidgets import QApplication, QWidget, QLabel, QHBoxLayout
+from PySide2.QtSvg import QSvgWidget
+
+
+sys.path.append(Path(__file__).parents[1].joinpath("lib").as_posix())
+
+from moonapi.moon_phase import MoonPhaseApi
+from moonapi.exceptions import MoonPhaseApiError
+
+class MoonPhaseWidget(QSvgWidget):
+
+    def __init__(self, parent=None, posx=0, posy=0):
+        super(MoonPhaseWidget, self).__init__(parent)
+
+
+        self.parent = parent
+        self.position = (posx, posy)
+        
+        self.timer = QTimer()
+        # self.timer.timeout.connect(self.handleTimeout)
+        self.timer.start(1000 * 60)  # update every minute
+
+        self.moon_api = self._init_moon_api()
+        lat, long = MoonPhaseWidget.get_lat_long("Vienna")
+        result = self.moon_api.moon_phase(lat=lat, long=long, date=dt.datetime.today(), format="svg")
+        destination = self.moon_api.download_image(result["data"]["imageUrl"])
+        self.load(destination.as_posix())
+
+
+    @staticmethod
+    @cached(LRUCache(maxsize=128))
+    def get_lat_long(city: str):
+        loc = Nominatim(user_agent="Geopy Library")
+        getLoc = loc.geocode(city)
+
+        lat, long = (
+            getLoc.latitude,
+            getLoc.longitude,
+        )
+        return lat, long
+
+    def _init_moon_api(self):
+        APP_ID = os.getenv("APPLICATION_ID")
+        APP_SECRET = os.getenv("APPLICATION_SECRET")
+        return MoonPhaseApi(APP_ID, APP_SECRET)
+
+
+    # def paintEvent(self, event: QPaintEvent) -> None:
+    #     self.paintMoonPhase()
+
+   
+
+    # def paintMoonPhase(self):
+    #     painter = QPainter(self)
+
+    #     pen = QPen()
+    #     pen.setColor(Qt.green)
+    #     painter.setPen(Qt.black)
+    #     # painter.setFont(QFont("Arial", min(self.height(), self.width()) - 60))
+    #     font = QFont("Helvetica")
+    #     font.setPixelSize(self.fontsize)
+    #     painter.setFont(font)
+
+
+    
+    # def event(self, event: QEvent) -> bool:
+    #     # print(event.type())
+    #     return super().event(event)
+    
+    # @Slot()
+    # def handleTimeout(self):
+    #     # self.paint()
+    #     self.update()
+
+
+class Window(QWidget):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.setWindowTitle("Smartclock")
+        # self.setGeometry(100, 100, 1920, 1080)
+        self.setGeometry(100, 100, 840, 480)
+
+        layout = QHBoxLayout()
+
+        digit = MoonPhaseWidget(parent=self, posx=20, posy=20)
+
+        layout.addWidget(digit)
+        self.setLayout(layout)
+
+        self.show()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    # clock = FlipClock()
+    window = Window()
+
+    sys.exit(app.exec_())
--- tests/test_moonphase.py
+++ tests/test_moonphase.py
@@ -10,7 +10,7 @@ from geopy.geocoders import Nominatim
 
 sys.path.append(Path(__file__).parents[1].joinpath("lib").as_posix())
 
-from moonapi.moon_phase import MoonPhaseApi
+from clock2py.moonapi.moon_phase import MoonPhaseApi
 
 load_dotenv()
 loc = Nominatim(user_agent="Geopy Library")
@@ -57,6 +57,50 @@ class TestMoonPhaseApi(unittest.TestCase):
 
         self.assertTrue(isinstance(result, dict))
         self.assertTrue("imageUrl" in result.get("data", {}))
+
+    def test_download(self):
+        api = MoonPhaseApi(app_id=APP_ID, app_secret=APP_SECRET)
+        lat, long = (
+            getLoc.latitude,
+            getLoc.longitude,
+        )
+        result = api.moon_phase(lat=lat, long=long, date="2024-01-31")
+
+        self.assertTrue(isinstance(result, dict))
+        self.assertTrue("imageUrl" in result.get("data", {}))
+        url = result.get("data", {}).get("imageUrl")
+
+        image_path = api.download_image(url)
+        self.assertTrue(image_path.exists())
+        self.assertTrue(image_path.suffix.endswith("png"))
+        with open(image_path, "rb") as f:
+            data = f.read()
+            self.assertTrue(len(data) > 0)
+
+        ## svg
+        result = api.moon_phase(lat=lat, long=long, date="2024-01-31", format="svg")
+
+        self.assertTrue(isinstance(result, dict))
+        self.assertTrue("imageUrl" in result.get("data", {}))
+        url = result.get("data", {}).get("imageUrl")
+
+        image_path = api.download_image(url)
+        self.assertTrue(image_path.exists())
+        self.assertTrue(image_path.suffix.endswith("svg"))
+        with open(image_path, "rb") as f:
+            data = f.read()
+            self.assertTrue(len(data) > 0)
+
+    def test_body_positions(self):
+        api = MoonPhaseApi(app_id=APP_ID, app_secret=APP_SECRET)
+        lat, long = (
+            getLoc.latitude,
+            getLoc.longitude,
+        )
+        result = api.body_positions(lat=lat, long=long, elevation=100, time="19:30:00", from_date="2024-01-31", to_date="2024-01-31")
+
+        self.assertTrue(isinstance(result, dict))
+
 
 
 if __name__ == "__main__":
